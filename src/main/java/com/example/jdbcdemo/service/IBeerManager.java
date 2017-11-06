950248eb48a6701eed21827d540cf8b18b0b4828
Marek Przybolewski <mprzybolewski@gmail.com>
2017-11-06 11:41:54 +0100

    add addAllBeers method and deleteAllBeers method and tests to this methods

--- src/main/java/com/example/jdbcdemo/service/IBeerManager.java
+++ src/main/java/com/example/jdbcdemo/service/IBeerManager.java
@@ -6,10 +6,13 @@ import java.util.List;
 public interface IBeerManager {
 
     int addBeer(Beer beer);
-    int deleteBeer(int id);
+    int deleteBeer(String name);
     int updateBeer(Beer beer, int id);
     void clearBeers();
     List<Beer> getAllBeers();
     List<Beer> searchBeer(String name);
+    void addAllBeers(List<Beer> beers);
+    void deleteAllBeers(List<String> beersNames);
+
 
 }
--- src/test/java/com/example/jdbcdemo/service/BeerManagerTest.java
+++ src/test/java/com/example/jdbcdemo/service/BeerManagerTest.java
@@ -3,8 +3,11 @@ package com.example.jdbcdemo.service;
 import com.example.jdbcdemo.domain.Beer;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -23,8 +26,18 @@ public class BeerManagerTest {
 	private final static double PERCENTOFALCOHOL_2 = 5;
 	private final static double PRICE_2 = 4;
 
-	private final static String NAME_TO_UPDATE = "Test";
-	private final static String TYPE_TO_UPDATE = "Test";
+	private final static String NAME_3 = "Zywiec Apa";
+	private final static String TYPE_3 = "Apa";
+	private final static double PERCENTOFALCOHOL_3 = 5;
+	private final static double PRICE_3 = 4;
+
+	private final static String NAME_4 = "Zywiec Sajson";
+	private final static String TYPE_4 = "Sajson";
+	private final static double PERCENTOFALCOHOL_4 = 5;
+	private final static double PRICE_4 = 4;
+
+	private final static String NAME_TO_UPDATE = "Zywiec Bock";
+	private final static String TYPE_TO_UPDATE = "Bock";
 	private final static double PERCENT_TO_UPDATE = 1;
 	private final static double PRICE_TO_UPDATE = 1;
 	
@@ -96,7 +109,7 @@ public class BeerManagerTest {
 
 	@Test
 	public void checkDeleting(){
-		Beer beer1 = new Beer(NAME_1, TYPE_2, PERCENTOFALCOHOL_1, PRICE_1);
+		Beer beer1 = new Beer(NAME_1, TYPE_1, PERCENTOFALCOHOL_1, PRICE_1);
 
 		beerManager.clearBeers();
 		beerManager.addBeer(beer1);
@@ -104,7 +117,59 @@ public class BeerManagerTest {
 		List<Beer> beers = beerManager.searchBeer("Zywiec Marcowe");
 		Beer beerRetrieved = beers.get(0);
 
-		assertEquals(1,beerManager.deleteBeer(beerRetrieved.getId()));
-		
+		assertEquals(1,beerManager.deleteBeer(beerRetrieved.getName()));
+	}
+
+	@Test
+	public void checkAddAllBeers(){
+		Beer beer1 = new Beer(NAME_1, TYPE_2, PERCENTOFALCOHOL_1, PRICE_1);
+		Beer beer2 = new Beer(NAME_2, TYPE_2, PERCENTOFALCOHOL_2, PRICE_2);
+		Beer beer3 = new Beer(NAME_3, TYPE_3, PERCENTOFALCOHOL_3, PRICE_3);
+		Beer beer4 = new Beer(NAME_4, TYPE_4, PERCENTOFALCOHOL_4, PRICE_4);
+
+		beerManager.clearBeers();
+
+		List<Beer> beers = new ArrayList<>();
+		beers.add(beer1);
+		beers.add(beer2);
+		beers.add(beer3);
+		beers.add(beer4);
+
+		beerManager.addAllBeers(beers);
+
+		int size = beerManager.getAllBeers().size();
+
+		assertThat(size, either(is(4)).or(is(0)));
+
+	}
+
+	@Test
+	public void checkDeleteAllBerrs(){
+		Beer beer1 = new Beer(NAME_1, TYPE_2, PERCENTOFALCOHOL_1, PRICE_1);
+		Beer beer2 = new Beer(NAME_2, TYPE_2, PERCENTOFALCOHOL_2, PRICE_2);
+		Beer beer3 = new Beer(NAME_3, TYPE_3, PERCENTOFALCOHOL_3, PRICE_3);
+		Beer beer4 = new Beer(NAME_4, TYPE_4, PERCENTOFALCOHOL_4, PRICE_4);
+
+		beerManager.clearBeers();
+		beerManager.addBeer(beer1);
+		beerManager.addBeer(beer2);
+		beerManager.addBeer(beer3);
+		beerManager.addBeer(beer4);
+
+		List<String> beersNames = new ArrayList<>();
+
+		beersNames.add(beer1.getName());
+		beersNames.add(beer2.getName());
+		beersNames.add(beer3.getName());
+		beersNames.add(beer4.getName());
+		beersNames.add("Test");
+
+		beerManager.deleteAllBeers(beersNames);
+		int size = beerManager.getAllBeers().size();
+		assertThat(size, either(is(4)).or(is(0)));
+		System.out.println("Size: " + size);
+
+
+
 	}
 }
